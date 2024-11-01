d93c0c0882251cb746f9d68b164be88511f03a34
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2024-11-01 19:53:28 +0100

    Move scripts from friction repo
    And other minor.

---
+++ build_ico.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Friction - https://friction.graphics
+#
+# Copyright (c) Ole-André Rodlie and contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+set -e -x
+
+PNG=`pwd`/hicolor/256x256/apps/graphics.friction.Friction.png
+ICO=`pwd`/friction.ico
+convert -background transparent ${PNG} -define icon:auto-resize="256,128,96,64,48,32,16" ${ICO}
+
